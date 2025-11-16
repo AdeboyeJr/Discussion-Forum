e88e6efbb44f5f8cf70eeb8669f79270e5d47d18
Adeboye Adegbenro Jr <adeboyejr@gmail.com>
2025-11-16 09:44:38 -0500

    Modify database credentials in db_include.php

--- db_include.php
+++ db_include.php
@@ -3,7 +3,7 @@ function doDB() {
     global $mysqli;
 
     // connect to server and select database; you may need it
-    $mysqli = mysqli_connect("localhost", "u667897109_Ade", "T#st1125", "u667897109_Data");
+    $mysqli = mysqli_connect("localhost", "u667897109_adejr", "D#vstack11", "u667897109_web_collection");
 
     // if connection faills, stop script execution
     if (mysqli_connect_errno()) {
